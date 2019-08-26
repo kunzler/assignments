121ab717fc3b741782a5d440a925ce7b2c4b814f
Christopher Kunzler <ckunzler@gmail.com>
2019-08-25 22:56:39 -0600

    assignments completed

---
+++ excercises/loop-olympics.js
@@ -0,0 +1,17 @@
+// Write a for loop that prints to the console the numbers 0 through 9.
+// for (i = 0; i < 10; i++ ){
+//     console.log(i);
+// } 
+//Write a for loop that prints to the console 9 through 0.
+for (i = 9; i >= 0; i-- ){
+    console.log(i);
+}
+
+// Write a for loop that prints these fruits to the console.
+ var fruit = ["banana", "orange", "apple", "kiwi"]
+
+fruit.forEach((x) => { 
+   console.log(x); 
+});
+
+//fruit.forEach(console.log);
